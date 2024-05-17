60a3456b469d0b62cee8db844a65f720aeff59e1
saurabhguptaiit27 <saurabhguptaiit27@gmail.com>
2024-05-17 20:43:42 +0530

    WIP

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://csmock.vercel.app"],
+    origin: ["http://localhost:5173", "https://csmock.vercel.app/"],
     credentials: true,
     optionsSuccessStatus: 200,
   })
