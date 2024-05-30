f1d7ec3110ebddca4ff718b94c8771684beba7c7
saurabhguptaiit27 <saurabhguptaiit27@gmail.com>
2024-05-30 20:07:35 +0530

    WIP...cookie in req.body

--- src/middlewares/userauth.middleware.js
+++ src/middlewares/userauth.middleware.js
@@ -6,9 +6,11 @@ import { User } from "../models/user.models.js";
 export const verifyJWT = asyncHandler(async (req, _, next) => {
   try {
     const token =
+      req.body.accessToken ||
       req.cookies?.accessToken ||
       req.header("Authorization")?.replace("Bearer ", "");
 
+    console.log("req.body ", req.body);
     console.log("req.cookies ", req.cookies);
     console.log('req.header("Authorization") ', req.header("Authorization"));
 
