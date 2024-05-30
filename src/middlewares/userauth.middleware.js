cbb197f3599a73958905a8f4ebc752e7661d363f
saurabhguptaiit27 <saurabhguptaiit27@gmail.com>
2024-05-30 19:31:18 +0530

    WIP...console

--- src/middlewares/userauth.middleware.js
+++ src/middlewares/userauth.middleware.js
@@ -9,6 +9,9 @@ export const verifyJWT = asyncHandler(async (req, _, next) => {
       req.cookies?.accessToken ||
       req.header("Authorization")?.replace("Bearer ", "");
 
+    console.log("req.cookies ", req.cookies);
+    console.log('req.header("Authorization") ', req.header("Authorization"));
+
     if (!token) {
       throw new ApiError(401, "Unauthorized request");
     }
